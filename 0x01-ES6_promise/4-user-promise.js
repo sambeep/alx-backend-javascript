9c58d19c923104b3c2e1e799e7f7918c0b215662
sambeep <samsonkinyanjui11@gmail.com>
2024-10-03 11:29:07 +0300

    javascript

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+export default function signUpUser(firstName, lastName) {
+    return Promise.resolve(
+      {
+        firstName,
+        lastName,
+      },
+    );
+  }
+  
---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,6 @@
+
+export default function uploadPhoto(filename) {
+    return Promise.reject(
+      new Error(`${filename} cannot be processed`),
+    );
+  }
