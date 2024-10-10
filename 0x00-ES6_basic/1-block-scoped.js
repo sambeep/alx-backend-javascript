ab2ece7536fbd57b6439bf449bb1b818364b0a07
sambeep <samsonkinyanjui11@gmail.com>
2024-10-10 12:49:27 +0300

    fix error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      // eslint-disable-next-line
-      const task = true;
-      // eslint-disable-next-line
-      const task2 = false;
-    }
-  
-    return [task, task2];
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
   }
+
+  return [task, task2];
+}
