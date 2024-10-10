2dd1a9a235c4e60e9680b212126f2756f5643252
sambeep <samsonkinyanjui11@gmail.com>
2024-10-10 12:14:30 +0300

    typescript

---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,11 @@
+s (8 sloc)  163 Bytes
+
+namespace Subjects {
+    export class Subject {
+      public teacher: Teacher;
+
+      setTeacher(value: Teacher) {
+        this.teacher = value;
+      }
+    }
+  }
