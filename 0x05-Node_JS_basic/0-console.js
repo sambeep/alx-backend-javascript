49b7566075bdc0552f0a69166915df5a443728a1
sambeep <samsonkinyanjui11@gmail.com>
2024-11-29 12:20:26 +0300

    nodejs

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Serah Njoroge <https://github.com/njoroge-s>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+    
+  module.exports = displayMessage;
